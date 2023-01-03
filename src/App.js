deb323871e2a19dc2bebba73f7267d118e969428
Wassim <wassimchakib97@gmail.com>
2023-01-03 11:24:37 +0100

    Add font-awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <>
+      <Sidebar />
+      <h1>Hello World</h1>
+    </>
   );
 }
 
